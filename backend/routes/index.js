70872179a8d2cbdc0148a9b44a920fe5149920b7
Erik <slicedblue@gmail.com>
2022-03-23 00:25:38 -0400

    reverted last commit, changes were not working

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,19 +3,17 @@ const axios = require("axios");
 const express = require("express");
 const router = express.Router();
 
-const api = axios.create({
-    headers: {
-        "X-Riot-Token": config.get("leagueApiKey")
-    }
-});
-
 router.get("/", (req, res) => {
     res.status(200).send("API Route");
 })
 
 router.get("/summoner/:name", async (req, res) => {
     const name = req.params.name;
-    const response = await api.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}`);
+    const response = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}`, {
+        headers: {
+            "X-Riot-Token": config.get("leagueApiKey")
+        }
+    });
     res.send(response.data);
 })
 
