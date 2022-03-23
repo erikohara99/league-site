91690f3e7fbb8967f84d536dc58e849d3793215e
Erik <slicedblue@gmail.com>
2022-03-23 00:19:14 -0400

    api working, key added in header of requests

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,20 @@
+const config = require("config");
+const axios = require("axios");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.status(200).send("Hello World!");
+    res.status(200).send("API Route");
+})
+
+router.get("/summoner/:name", async (req, res) => {
+    const name = req.params.name;
+    const response = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}`, {
+        headers: {
+            "X-Riot-Token": config.get("leagueApiKey")
+        }
+    });
+    res.send(response.data);
 })
 
 module.exports = router;
