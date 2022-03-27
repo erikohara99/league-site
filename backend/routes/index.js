cc03ddab22a837a643e8a50d6bd6c96877e0f028
Erik <slicedblue@gmail.com>
2022-03-27 17:47:34 -0400

    added new API endpoint that returns both summoner and matches from summoner, /summoner/matches/:name

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,18 +6,20 @@ const router = express.Router();
 // Set API key to every request to clean up code
 axios.defaults.headers.common["X-Riot-Token"] = config.get("leagueApiKey");
 
-router.get("/", (req, res) => {
-    res.status(200).send("API Route");
-})
-
 router.get("/summoner/:name", async (req, res) => {
     const name = req.params.name;
     const response = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}`);
     res.send(response.data);
 })
 
-router.get("/summoner/matches", async (req, res) => {
-    const response = await axios.get();
+router.get("/summoner/matches/:name", async (req, res) => {
+    const name = req.params.name;
+    let responses = {};
+    const summoner = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}`);
+    responses.summoner = summoner.data;
+    const matches = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${responses.summoner.puuid}/ids?start=0&count=20`);
+    responses.matches = matches.data;
+    res.send(responses);
 })
 
 module.exports = router;
