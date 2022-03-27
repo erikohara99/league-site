f815681474a40cd4c9df64a2c195ebc81c727eb9
Erik <slicedblue@gmail.com>
2022-03-27 18:22:28 -0400

    changed css of profiles a bit more

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -8,7 +8,7 @@ function Profile(props) {
         <div className="profile">
             <img src={`http://ddragon.leagueoflegends.com/cdn/12.5.1/img/profileicon/${profileIconId}.png`} alt="profile icon for user"></img>
             <h1 className="profile-level">{summonerLevel}</h1>
-            <h1>{name}</h1>
+            <h1 className="profile-name">{name}</h1>
         </div>
      );
 }
