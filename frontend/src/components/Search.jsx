bf1cde3ec23d2e13fb1b2a44a8722fbfee40b1ce
Erik <slicedblue@gmail.com>
2022-03-28 16:09:13 -0400

    started splash at top of landing page

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import LandingVideo from "../video/landing.mp4";
 
 function Search(props) {
     return ( 
         <div className="searchbox">
-            <h1>StatChecked</h1>
+            <div id="searchbox-name">
+                <h1>StatChecked</h1>
+                <div>
+                    <h2>All your stats.</h2>
+                    <h2>All your answers.</h2>
+                </div>
+            </div>
             <form onSubmit={props.onClick}>
                 <input id="searchbox-input" type="text" name="username" placeholder="League username..."></input>
                 <button id="searchbox-button"type="submit">Search</button>
